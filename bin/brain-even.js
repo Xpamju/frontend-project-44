0fa7943d6daa0e5e85bce533f5b7d510afadc1cd
Julia Khramchenkova <Pineapple05955@gmail.com>
2022-12-08 01:33:37 +0300

    vern

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello ,' + userName + '!');
+// console.log('Welcome to the Brain Games!');
+// const userName = readlineSync.question('May I have your name? ');
+// console.log('Hello ,' + userName + '!');
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
+// console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
-let answ = '\nYour answer: ';
+
 
 
 function play() {
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hello ,' + userName + '!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
+
+let answ = '\nYour answer: ';
 let useranswer = ''
 let yes = 'yes'
 let no = 'no'
@@ -19,27 +25,28 @@ function num() {
     let number = (Math.round(Math.random() * 100));
       return number;
     }
+  
     
 const quest = readlineSync.question('Question: ' + num() + answ + useranswer);
 
-let truly = 'Correct!';
-let falsy = (useranswer + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + userName);
+const quest2 = (`${quest} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}.`);
 
-    let newLocal = function ifa() {
-        if (num() % 2 === 0 && quest === yes) {
-            return truly;
-        } if (num() % 2 === 0 && quest === no) {
-            return falsy;
-        } if (num() % 2 !== 0 && quest === yes) {
-            return falsy;
-        } if (num() % 2 !== 0 && quest === no) {
-            return(truly);
-        } else {
-            return falsy;
-        }
+function ifa() {
+const quest2 = (`${quest} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}.`);
+let truly = 'Correct!';
+    if (num() % 2 === 0 && quest === yes) {
+        console.log(truly)
+    } if (num() % 2 === 0 && quest === no) {
+        return quest2;
+    } if (num() % 2 !== 0 && quest === yes) {
+        return quest2;
+    } if (num() % 2 !== 0 && quest === no) {
+        return truly;
+    } else {
+        return falsy;
     }
-    return newLocal;
-    
+    }
+return ifa;
 }
 
 play()
