0b7722a7931c0a0025bcb55b6446f942b34279b6
Julia Khramchenkova <Pineapple05955@gmail.com>
2022-12-10 17:28:42 +0300

    no mist

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
- function greeting(){
+
     console.log('Welcome to the Brain Games!');
     const userName = readlineSync.question('May I have your name? ');
     console.log('Hello ,' + userName + '!');
-}
-greeting()
 
