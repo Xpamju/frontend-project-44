35ed4159884f425d5b817b4670d44ce6d7f27519
Julia Khramchenkova <Pineapple05955@gmail.com>
2022-12-18 14:44:33 +0300

    cli

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,14 +1,3 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-
-function getName() {
- const userName = readlineSync.question('May I have your name? ');
- return userName;
-}
- 
-function brainGame() {
-  console.log('Welcome to the Brain Games!');
-  const user = getName();
-  console.log(`Hello, ${user}!`)
-}
+import brainGame from '../src/cli.js';
 brainGame();
