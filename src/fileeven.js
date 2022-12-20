bcf8a3c2dfff2480dbe6f04aa50deb01d23a904d
Julia Khramchenkova <Pineapple05955@gmail.com>
2022-12-20 11:41:26 +0300

    even fix !

--- src/fileeven.js
+++ src/fileeven.js
@@ -13,11 +13,11 @@ export default function game() {
   for (let i = 0; i < 3; i += 1) {
     const numsave = num();
     const quest = readlineSync.question('Question: ' + numsave + '\nYour answer: ');
-    if ((numsave % 2 === 0 && quest === 'yes') || (numsave % 2 !== 0 && quest === 'no')) {
-      console.log('Coorect!');
-    } else {
-      console.log(`${quest} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}.`);
-      return;
+  if ((numsave % 2 === 0 && quest === 'yes') || (numsave % 2 !== 0 && quest === 'no')) {
+    console.log('Coorect!');
+  } else {
+    console.log(`${quest} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+    return;
     }
   }
   console.log(`Congratulations, ${userName}!`);
